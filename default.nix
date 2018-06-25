266a698ef2a6f297d772a1a221c83d0c1df76cc6
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-06-25 12:03:09 +0200

    add: introduce recursive versionning

--- default.nix
+++ default.nix
@@ -1,4 +1,15 @@
 { pkgs ? import <nixpkgs> {} }: with pkgs;
+let
+  mkOlderVersion = { rev, sha256 }:
+    let
+      olderVersion = fetchFromGitHub {
+        owner = "fretlink";
+        repo  = "clever-tools-nix";
+        inherit rev;
+        inherit sha256;
+      };
+    in (import olderVersion {}).latest;
+in
 {
   latest = callPackage ./clever-tools {
              nodegit = callPackage ./nodegit {};
